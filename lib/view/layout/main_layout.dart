6e982cb00f9ecde4c6dcf35100e88252d6e9ee0b
Barba2k2 <lorenzo.dz@hotmail.com>
2025-06-28 10:34:34 -0300

    feat(layout): add main layout view

---
+++ lib/view/layout/main_layout.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:paintpro/view/widgets/navigation/floating_bottom_navigation_bar.dart';
+import 'package:paintpro/viewmodel/navigation_viewmodel.dart';
+
+class MainLayout extends StatefulWidget {
+  final Widget child;
+  final String currentRoute;
+
+  const MainLayout({
+    super.key,
+    required this.child,
+    required this.currentRoute,
+  });
+
+  @override
+  State<MainLayout> createState() => _MainLayoutState();
+}
+
+class _MainLayoutState extends State<MainLayout> {
+  late NavigationViewModel _navigationViewModel;
+
+  @override
+  void initState() {
+    super.initState();
+    _navigationViewModel = NavigationViewModel();
+    _navigationViewModel.updateCurrentRoute(widget.currentRoute);
+  }
+
+  @override
+  void didUpdateWidget(MainLayout oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.currentRoute != widget.currentRoute) {
+      _navigationViewModel.updateCurrentRoute(widget.currentRoute);
+    }
+  }
+
+  @override
+  void dispose() {
+    _navigationViewModel.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<NavigationViewModel>.value(
+      value: _navigationViewModel,
+      child: Scaffold(
+        body: widget.child,
+        extendBody: true,
+        bottomNavigationBar: Consumer<NavigationViewModel>(
+          builder: (context, viewModel, child) {
+            return FloatingBottomNavigationBar(
+              viewModel: viewModel,
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
