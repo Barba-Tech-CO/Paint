169b56093ed9147299f58bf457789c5c96b89f3f
Barba2k2 <lorenzo.dz@hotmail.com>
2025-06-28 12:51:45 -0300

    feat(service): add IHttpService contract for HTTP abstraction

---
+++ lib/service/i_http_service.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+
+abstract class IHttpService {
+  Future<Response> get(String path, {Map<String, dynamic>? queryParameters});
+  Future<Response> post(
+    String path, {
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+  });
+  Future<Response> put(
+    String path, {
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+  });
+  Future<Response> delete(
+    String path, {
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+  });
+}
