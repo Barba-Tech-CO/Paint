8ff8a4127fe3151173aca9a291942eb3fa18ce83
Barba2k2 <lorenzo.dz@hotmail.com>
2025-06-28 10:34:49 -0300

    feat(viewmodel): add navigation viewmodel

---
+++ lib/viewmodel/navigation_viewmodel.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/foundation.dart';
+import 'package:paintpro/model/navigation_item_model.dart';
+
+class NavigationViewModel extends ChangeNotifier {
+  String _currentRoute = '/';
+  final List<NavigationItemModel> _navigationItems =
+      NavigationItemModel.defaultItems;
+
+  String get currentRoute => _currentRoute;
+  List<NavigationItemModel> get navigationItems => _navigationItems;
+
+  int get currentIndex {
+    final index = _navigationItems.indexWhere(
+      (item) => item.route == _currentRoute,
+    );
+    return index != -1 ? index : 0;
+  }
+
+  bool isActiveRoute(String route) {
+    return _currentRoute == route;
+  }
+
+  void updateCurrentRoute(String route) {
+    if (_currentRoute != route) {
+      _currentRoute = route;
+      notifyListeners();
+    }
+  }
+
+  NavigationItemModel? getItemByRoute(String route) {
+    try {
+      return _navigationItems.firstWhere((item) => item.route == route);
+    } catch (e) {
+      return null;
+    }
+  }
+
+  NavigationItemModel getItemByIndex(int index) {
+    if (index >= 0 && index < _navigationItems.length) {
+      return _navigationItems[index];
+    }
+    return _navigationItems.first;
+  }
+}
