d78de7f2e5e028130d3441e531e8b7156050d868
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-06-30 09:50:27 -0300

    fix: server ports

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -5,9 +5,9 @@ class AppConfig {
     if (Platform.isAndroid) {
       return 'http://10.0.2.2:3000'; // Porta padrão, ajuste se necessário
     } else if (Platform.isIOS) {
-      return 'http://localhost:3000';
+      return 'http://localhost:8000';
     } else {
-      return 'http://localhost:3000';
+      return 'http://localhost:8000';
     }
   }
 }
