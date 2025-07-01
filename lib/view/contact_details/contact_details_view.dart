4388dc6ffc34acf22771f76473990efbbc316675
Gabriel <gabrielvasco906@gmail.com>
2025-07-01 12:06:55 -0300

    feat: implement ContactDetailsView layout with contact information

--- lib/view/contact_details/contact_details_view.dart
+++ lib/view/contact_details/contact_details_view.dart
@@ -5,6 +5,45 @@ class ContactDetailsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Detalhes do Contato'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Column(
+          spacing: 4,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 24),
+            Text('Sebastião'),
+            Text('Marcos Ferreira'),
+            Text('Masculino'),
+            SizedBox(height: 32),
+            Text("sebastiao_ferreira@live.com.br"),
+            Text("(65) 99268-1400"),
+            SizedBox(height: 32),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Rua Seis"),
+                Text("78055-865"),
+              ],
+            ),
+            Text("Cuiabá"),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Mato Grosso"),
+                Text("Brasil"),
+              ],
+            ),
+            SizedBox(height: 24),
+            Text("LocationId"),
+            Text("Pietro e Caroline Ferragens Ltda"),
+          ],
+        ),
+      ),
+    );
   }
 }
