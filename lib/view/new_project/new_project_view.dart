1020ac516b37399ebf09f407567c61cf9a9d0516
Gabriel <gabrielvasco906@gmail.com>
2025-07-02 15:24:41 -0300

    feat: refactor NewProjectView to StatefulWidget and add InputCardWidget for form inputs

--- lib/view/new_project/new_project_view.dart
+++ lib/view/new_project/new_project_view.dart
@@ -1,15 +1,159 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:paintpro/config/app_colors.dart';
 import 'package:paintpro/view/widgets/appbars/app_bar_widget.dart';
+import 'package:paintpro/view/widgets/cards/input_card_widget.dart';
 
-class NewProjectView extends StatelessWidget {
+class NewProjectView extends StatefulWidget {
   const NewProjectView({super.key});
+
+  @override
+  State<NewProjectView> createState() => _NewProjectViewState();
+}
+
+class _NewProjectViewState extends State<NewProjectView> {
+  final TextEditingController _projectNameController = TextEditingController();
+  final TextEditingController _additionalNotesController =
+      TextEditingController();
+
+  // Estado para controlar a seleção do tipo de projeto
+  String _selectedProjectType = 'Interior';
+
+  @override
+  void dispose() {
+    _projectNameController.dispose();
+    _additionalNotesController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarWidget(title: 'New Project'),
-      body: Column(
-        children: [],
+      backgroundColor: AppColors.background,
+      appBar: AppBarWidget(
+        title: 'New Project',
+        leading: Row(
+          children: [
+            IconButton(
+              icon: const Icon(Icons.arrow_back_ios),
+              onPressed: () => context.pop(),
+            ),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            spacing: 12,
+            children: [
+              const SizedBox.shrink(),
+
+              // Card de Notas Adicionais
+              InputCardWidget(
+                title: 'Client Information',
+                description: 'Client Name*',
+                controller: _additionalNotesController,
+                hintText: 'John',
+                maxLines: 1,
+              ),
+
+              InputCardWidget(
+                title: 'Project Details',
+                description: 'Project Name *',
+                controller: _projectNameController,
+                hintText: 'Enter project name',
+                widget: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8),
+                      child: Text(
+                        'Project Type *',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          color: AppColors.textPrimary,
+                        ),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Radio(
+                          value: 'Interior',
+                          groupValue: _selectedProjectType,
+                          onChanged: (value) {
+                            setState(() {
+                              _selectedProjectType = value.toString();
+                            });
+                          },
+                          activeColor: AppColors.primary,
+                        ),
+                        Text('Interior'),
+
+                        const SizedBox(width: 16),
+
+                        Radio(
+                          value: 'Exterior',
+                          groupValue: _selectedProjectType,
+                          onChanged: (value) {
+                            setState(() {
+                              _selectedProjectType = value.toString();
+                            });
+                          },
+                          activeColor: AppColors.primary,
+                        ),
+                        Text('Exterior'),
+
+                        const SizedBox(width: 16),
+
+                        Radio(
+                          value: 'Both',
+                          groupValue: _selectedProjectType,
+                          onChanged: (value) {
+                            setState(() {
+                              _selectedProjectType = value.toString();
+                            });
+                          },
+                          activeColor: AppColors.primary,
+                        ),
+                        Text('Both'),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+
+              InputCardWidget(
+                title: 'Additional Notes',
+                controller: _additionalNotesController,
+                hintText: 'Enter additional notes',
+                maxLines: 6,
+                multiline: true,
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
+        child: ElevatedButton(
+          onPressed: () {
+            // Lógica para criar o projeto
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: AppColors.primary,
+            foregroundColor: AppColors.textOnPrimary,
+            minimumSize: const Size(double.infinity, 48),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: const Text(
+            'Create Project',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+        ),
       ),
     );
   }
