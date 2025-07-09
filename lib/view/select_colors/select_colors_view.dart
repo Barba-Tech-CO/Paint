efb55759695979f143f26f22d4bf546cdfa47224
Gabriel <gabrielvasco906@gmail.com>
2025-07-09 12:52:37 -0300

    feat: refactor SelectColorsView to StatefulWidget and enhance PaintProAppBar with tab support

--- lib/view/select_colors/select_colors_view.dart
+++ lib/view/select_colors/select_colors_view.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:paintpro/view/widgets/appbars/paint_pro_app_bar.dart';
 
-class SelectColorsView extends StatelessWidget {
+class SelectColorsView extends StatefulWidget {
   const SelectColorsView({super.key});
+
+  @override
+  State<SelectColorsView> createState() => _SelectColorsViewState();
+}
+
+class _SelectColorsViewState extends State<SelectColorsView> {
+  late TabController _tabController;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PaintProAppBar(title: 'Select Colors'),
+      appBar: PaintProAppBar(
+        title: 'Select Colors',
+      ),
+      body: Column(
+        children: [],
+      ),
     );
   }
 }
