c3dca28b5bf47564f11da1c238199a3002912602
Barba2k2 <lorenzo.dz@hotmail.com>
2025-07-16 15:13:17 -0300

    feat(config): add app_urls.dart for centralized URL constants

---
+++ lib/config/app_urls.dart
@@ -0,0 +1,8 @@
+class AppUrls {
+  static const String ghlAuthorizeUrl =
+      'https://marketplace.gohighlevel.com/oauth/chooselocation?response_type=code&redirect_uri=https%3A%2F%2Fpaintpro.barbatech.company%2Fapi%2Foauth%2Fcallback&client_id=6845ab8de6772c0d5c8548d7-mbnty1f6&scope=contacts.write+associations.write+associations.readonly+oauth.readonly+oauth.write+invoices%2Festimate.write+invoices%2Festimate.readonly+invoices.readonly+associations%2Frelation.write+associations%2Frelation.readonly+contacts.readonly+invoices.write';
+
+  static const String deepLinkSuccess = 'paintproapp://auth/success';
+  static const String deepLinkError = 'paintproapp://auth/error';
+  // Adicione outras URLs conforme necessário
+}
