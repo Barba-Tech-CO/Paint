5f2e91d9ba3f5b5a6f705517d2091f25403b4a86
Barba2k2 <lorenzo.dz@hotmail.com>
2025-07-16 15:14:13 -0300

    feat(model): add auth_model model

---
+++ lib/model/auth_model/auth_model.dart
@@ -0,0 +1,33 @@
+class AuthModel {
+  final bool authenticated;
+  final DateTime? expiresAt;
+  final bool needsLogin;
+  final String? locationId;
+
+  AuthModel({
+    required this.authenticated,
+    this.expiresAt,
+    required this.needsLogin,
+    this.locationId,
+  });
+
+  factory AuthModel.fromJson(Map<String, dynamic> json) {
+    return AuthModel(
+      authenticated: json['authenticated'] ?? false,
+      expiresAt: json['expires_at'] != null
+          ? DateTime.parse(json['expires_at'])
+          : null,
+      needsLogin: json['needs_login'] ?? true,
+      locationId: json['location_id'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'authenticated': authenticated,
+      'expires_at': expiresAt?.toIso8601String(),
+      'needs_login': needsLogin,
+      'location_id': locationId,
+    };
+  }
+}
