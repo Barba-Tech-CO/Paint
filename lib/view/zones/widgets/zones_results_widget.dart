fb23c2c465b0aab724e8ad81c21ef1d9052cf19e
Gabriel <gabrielvasco906@gmail.com>
2025-07-23 20:04:37 -0300

    feat: implement ZonesSummaryCard and integrate it into ZonesResultsWidget

--- lib/view/zones/widgets/zones_results_widget.dart
+++ lib/view/zones/widgets/zones_results_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:paintpro/view/widgets/buttons/paint_pro_button.dart';
 import 'package:paintpro/view/zones/widgets/zones_card.dart';
+import 'package:paintpro/view/zones/widgets/zones_summary_card.dart';
 
 class ZonesResultsWidget extends StatelessWidget {
   final Map<String, dynamic> results;
@@ -13,33 +15,51 @@ class ZonesResultsWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Column(
-        children: [
-          const SizedBox(height: 16),
-          ZonesCard(
-            title: 'Kitchen',
-            image: "assets/images/kitchen.png",
-            valueDimension: '14 x 16',
-            valueArea: '224 sq ft',
-            valuePaintable: '631 sq ft',
-          ),
-          const SizedBox(height: 16),
-          ZonesCard(
-            title: 'Kitchen',
-            image: "assets/images/kitchen.png",
-            valueDimension: '14 x 16',
-            valueArea: '224 sq ft',
-            valuePaintable: '631 sq ft',
-          ),
-          const SizedBox(height: 16),
-          ZonesCard(
-            title: 'Kitchen',
-            image: "assets/images/kitchen.png",
-            valueDimension: '14 x 16',
-            valueArea: '224 sq ft',
-            valuePaintable: '631 sq ft',
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 16),
+            ZonesCard(
+              title: 'Kitchen',
+              image: "assets/images/kitchen.png",
+              valueDimension: '14 x 16',
+              valueArea: '224 sq ft',
+              valuePaintable: '631 sq ft',
+            ),
+            const SizedBox(height: 16),
+            ZonesCard(
+              title: 'Kitchen',
+              image: "assets/images/kitchen.png",
+              valueDimension: '14 x 16',
+              valueArea: '224 sq ft',
+              valuePaintable: '631 sq ft',
+            ),
+            const SizedBox(height: 16),
+            ZonesCard(
+              title: 'Kitchen',
+              image: "assets/images/kitchen.png",
+              valueDimension: '14 x 16',
+              valueArea: '224 sq ft',
+              valuePaintable: '631 sq ft',
+            ),
+            const SizedBox(height: 16),
+            ZonesSummaryCard(
+              avgDimensions: '14 x 16',
+              totalArea: '752 sq ft',
+              totalPaintable: '2031 sq ft',
+              onAdd: () {
+                // ação ao clicar no botão +
+              },
+            ),
+            const SizedBox(height: 32),
+            PaintProButton(
+              text: "Next",
+              onPressed: () {
+                // ação ao clicar no botão Next
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
