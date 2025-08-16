309d7c808c81b0dff98ed0aae4ccf17232c9e524
Lorenzo DZ <lorenzo.dz@hotmail.com>
2025-08-16 17:28:37 -0300

    feat: enhance photos gallery widget functionality

--- lib/view/edit_zone/widgets/photos_gallery_widget.dart
+++ lib/view/edit_zone/widgets/photos_gallery_widget.dart
@@ -85,7 +85,7 @@ class PhotosGalleryWidget extends StatelessWidget {
                     child: Container(
                       padding: const EdgeInsets.all(4),
                       decoration: BoxDecoration(
-                        color: Colors.black.withOpacity(0.6),
+                        color: Colors.black.withValues(alpha: 0.6),
                         borderRadius: BorderRadius.circular(12),
                       ),
                       child: const Icon(
