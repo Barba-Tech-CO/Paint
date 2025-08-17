2df7ea7723528ce0c2f38bcd4c43a94cf6db8df2
Barba2k2 <lorenzo.dz@hotmail.com>
2025-08-17 17:41:26 -0300

    feat: add camera feature with MVVM structure

---
+++ lib/features/camera/presentation/views/camera_view.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../../../config/app_colors.dart';
+import '../../../../view/widgets/appbars/paint_pro_app_bar.dart';
+
+class CameraView extends StatelessWidget {
+  const CameraView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      appBar: const PaintProAppBar(title: 'Camera'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InkWell(
+              onTap: () => context.push('/zones'),
+              child: Container(
+                width: 120,
+                height: 120,
+                decoration: BoxDecoration(
+                  color: AppColors.surface,
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(
+                    color: AppColors.divider,
+                    width: 2,
+                  ),
+                ),
+                child: Icon(
+                  Icons.add_a_photo_outlined,
+                  size: 48,
+                  color: AppColors.textSecondary,
+                ),
+              ),
+            ),
+            const SizedBox(height: 24),
+            Text(
+              'Camera',
+              style: GoogleFonts.albertSans(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: AppColors.textPrimary,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'Capture your photos here',
+              style: GoogleFonts.albertSans(
+                fontSize: 16,
+                color: AppColors.textSecondary,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
