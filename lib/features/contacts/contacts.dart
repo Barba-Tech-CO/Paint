52f698f303ed8bd90b69aeaa1a6e1bd071b3fa3d
Barba2k2 <lorenzo.dz@hotmail.com>
2025-08-17 13:27:48 -0300

    feat: add contacts feature barrel file

---
+++ lib/features/contacts/contacts.dart
@@ -0,0 +1,11 @@
+// Domain
+export 'domain/entities/contact_entity.dart';
+export 'domain/repositories/contact_repository.dart';
+
+// Presentation
+export 'presentation/viewmodels/contact_list_viewmodel.dart';
+export 'presentation/views/contacts_view.dart';
+export 'presentation/views/widgets/contact_item_widget.dart';
+export 'presentation/views/widgets/contacts_list_widget.dart';
+export 'presentation/views/widgets/empty_state_widget.dart';
+export 'presentation/views/widgets/search_field_widget.dart';
