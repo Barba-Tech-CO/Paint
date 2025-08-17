472a7ff02a97c63eb3a2fc2019d67e814d11d3e3
Barba2k2 <lorenzo.dz@hotmail.com>
2025-08-17 17:49:20 -0300

    feat: add info_card_widget.dart to contacts feature

---
+++ lib/features/contacts/presentation/widgets/info_card_widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class InfoCardWidget extends StatelessWidget {
+  final List<Widget> children;
+
+  const InfoCardWidget({
+    super.key,
+    required this.children,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 2,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: children,
+        ),
+      ),
+    );
+  }
+}
