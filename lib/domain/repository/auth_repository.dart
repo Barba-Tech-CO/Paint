f8abbcb2862384bed3424528e4fcbc6d718ffb28
Barba2k2 <lorenzo.dz@hotmail.com>
2025-08-17 12:43:02 -0300

    refactor: update auth repository interface

--- lib/domain/repository/auth_repository.dart
+++ lib/domain/repository/auth_repository.dart
@@ -1,18 +1,19 @@
-import '../../model/auth_model.dart';
 import '../../utils/result/result.dart';
+import '../../features/auth/domain/entities/auth_status_response_entity.dart';
+import '../../features/auth/domain/entities/auth_refresh_response_entity.dart';
 
 abstract class IAuthRepository {
   /// Verifica o status de autenticação
-  Future<Result<AuthStatusResponse>> getStatus();
+  Future<Result<AuthStatusResponseEntity>> getStatus();
 
   /// Obtém a URL de autorização
   Future<Result<String>> getAuthorizeUrl();
 
   /// Processa o callback de autorização
-  Future<Result<AuthRefreshResponse>> processCallback(String code);
+  Future<Result<AuthRefreshResponseEntity>> processCallback(String code);
 
   /// Renova o token de acesso
-  Future<Result<AuthRefreshResponse>> refreshToken();
+  Future<Result<AuthRefreshResponseEntity>> refreshToken();
 
   /// Verifica se está autenticado
   Future<Result<bool>> isAuthenticated();
