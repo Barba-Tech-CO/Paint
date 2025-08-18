8fce546161232ba40b3907f504497e30f00fb8fc
Barba2k2 <lorenzo.dz@hotmail.com>
2025-08-18 10:12:30 -0300

    feat: add material repository implementation

---
+++ lib/data/repository/material_repository_impl.dart
@@ -0,0 +1,34 @@
+import '../../domain/repository/material_repository.dart';
+import '../../model/models.dart';
+import '../../service/material_service.dart';
+import '../../utils/result/result.dart';
+
+class MaterialRepository implements IMaterialRepository {
+  final MaterialService _materialService;
+
+  MaterialRepository({
+    required MaterialService materialService,
+  }) : _materialService = materialService;
+
+  @override
+  Future<Result<List<MaterialModel>>> getAllMaterials() {
+    return _materialService.getAllMaterials();
+  }
+
+  @override
+  Future<Result<List<MaterialModel>>> getMaterialsWithFilter(
+    MaterialFilter filter,
+  ) {
+    return _materialService.getMaterialsWithFilter(filter);
+  }
+
+  @override
+  Future<Result<MaterialModel?>> getMaterialById(String id) {
+    return _materialService.getMaterialById(id);
+  }
+
+  @override
+  Future<Result<MaterialStatsModel>> getMaterialStats() {
+    return _materialService.getMaterialStats();
+  }
+}
