6ab8d5542cacd95e45d764bef7fd37001ca48552
Gabriel <gabrielvasco906@gmail.com>
2025-08-21 14:58:22 -0300

    feat: update initial route to splash screen

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -4,7 +4,7 @@ import '../model/models.dart';
 import '../view/views.dart';
 
 final router = GoRouter(
-  initialLocation: '/quotes',
+  initialLocation: '/splash',
   routes: [
     GoRoute(
       path: '/splash',
