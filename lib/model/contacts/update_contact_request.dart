4a5c7bc8ee83f09b880e938d0a385792be4ea50e
Barba2k2 <lorenzo.dz@hotmail.com>
2025-08-27 13:51:14 -0300

    feat: update update contact request model

--- lib/model/contacts/update_contact_request.dart
+++ lib/model/contacts/update_contact_request.dart
@@ -4,6 +4,14 @@ class UpdateContactRequest {
   final String? phone;
   final String? companyName;
   final String? address;
+  final String? city;
+  final String? state;
+  final String? postalCode;
+  final String? country;
+  final List<String>? additionalEmails;
+  final List<String>? additionalPhones;
+  final List<Map<String, dynamic>>? customFields;
+  final List<String>? tags;
 
   UpdateContactRequest({
     this.name,
@@ -11,15 +19,38 @@ class UpdateContactRequest {
     this.phone,
     this.companyName,
     this.address,
+    this.city,
+    this.state,
+    this.postalCode,
+    this.country,
+    this.additionalEmails,
+    this.additionalPhones,
+    this.customFields,
+    this.tags,
   });
 
   Map<String, dynamic> toJson() {
     final json = <String, dynamic>{};
-    if (name != null) json['name'] = name;
+
+    // Add name field (API expects name)
+    if (name != null && name!.isNotEmpty) {
+      json['name'] = name;
+    }
+
+    // Add other fields
     if (email != null) json['email'] = email;
     if (phone != null) json['phone'] = phone;
     if (companyName != null) json['companyName'] = companyName;
-    if (address != null) json['address'] = address;
+    if (address != null) json['address1'] = address; // API expects address1
+    if (city != null) json['city'] = city;
+    if (state != null) json['state'] = state;
+    if (postalCode != null) json['postalCode'] = postalCode;
+    if (country != null) json['country'] = country;
+    if (additionalEmails != null) json['additionalEmails'] = additionalEmails;
+    if (additionalPhones != null) json['additionalPhones'] = additionalPhones;
+    if (tags != null) json['tags'] = tags;
+    if (customFields != null) json['customFields'] = customFields;
+
     return json;
   }
 }
