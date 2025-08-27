ce01b52cd292381517ec3c79e272bb2288b0812f
Barba2k2 <lorenzo.dz@hotmail.com>
2025-08-26 22:22:30 -0300

    feat: update contact name splitting helper

--- lib/helpers/contacts/split_full_name.dart
+++ lib/helpers/contacts/split_full_name.dart
@@ -1,15 +1,8 @@
 Map<String, String> splitFullName(String fullName) {
   final trimmedName = fullName.trim();
   if (trimmedName.isEmpty) {
-    return {'firstName': '', 'lastName': ''};
+    return {'name': ''};
   }
 
-  final parts = trimmedName.split(' ');
-  if (parts.length == 1) {
-    return {'firstName': parts[0], 'lastName': ''};
-  }
-
-  final firstName = parts.first;
-  final lastName = parts.skip(1).join(' ');
-  return {'firstName': firstName, 'lastName': lastName};
+  return {'name': trimmedName};
 }
