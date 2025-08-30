76b4aaff6d263643dcb3672751ff06a94a9b16e0
Barba2k2 <lorenzo.dz@hotmail.com>
2025-08-30 16:33:12 -0300

    refactor: update material repository domain interface

--- lib/domain/repository/material_repository.dart
+++ lib/domain/repository/material_repository.dart
@@ -6,11 +6,16 @@ abstract class IMaterialRepository {
   Future<Result<List<MaterialModel>>> getAllMaterials();
 
   /// Busca materiais com filtros aplicados
-  Future<Result<List<MaterialModel>>> getMaterialsWithFilter(MaterialFilter filter);
+  Future<Result<List<MaterialModel>>> getMaterialsWithFilter(
+    MaterialFilter filter,
+  );
 
   /// Busca material por ID
   Future<Result<MaterialModel?>> getMaterialById(String id);
 
   /// Busca estatísticas dos materiais
   Future<Result<MaterialStatsModel>> getMaterialStats();
+
+  /// Busca marcas disponíveis
+  Future<Result<List<String>>> getAvailableBrands();
 }
