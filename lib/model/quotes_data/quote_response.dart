854a6f28c18813ee252b1bf51a42b9774dfb6991
Barba2k2 <lorenzo.dz@hotmail.com>
2025-09-05 22:35:24 -0300

    feat: update quote response model

--- lib/model/quotes_data/quote_response.dart
+++ lib/model/quotes_data/quote_response.dart
@@ -4,26 +4,56 @@ class QuoteResponse {
   final bool success;
   final QuoteModel quote;
   final String message;
-  final String? r2Url;
+  final String? url;
   final int? size;
 
   QuoteResponse({
     required this.success,
     required this.quote,
     required this.message,
-    this.r2Url,
+    this.url,
     this.size,
   });
 
   factory QuoteResponse.fromJson(Map<String, dynamic> json) {
+    // Validate required fields
+    if (json['success'] == null) {
+      throw Exception('Missing required field: success');
+    }
+
+    final data = json['data'] as Map<String, dynamic>?;
+    if (data == null) {
+      throw Exception('Missing required field: data');
+    }
+
+    if (data['upload'] == null) {
+      throw Exception('Missing required field: data.upload');
+    }
+
+    if (data['message'] == null) {
+      throw Exception('Missing required field: data.message');
+    }
+
     return QuoteResponse(
       success: json['success'] as bool,
       quote: QuoteModel.fromJson(
-        json['data']['upload'] as Map<String, dynamic>,
+        data['upload'] as Map<String, dynamic>,
       ),
-      message: json['data']['message'] as String,
-      r2Url: json['data']['r2_url'] as String?,
-      size: json['data']['size'] as int?,
+      message: data['message'] as String,
+      url: data['url'] as String?,
+      size: data['size'] as int?,
     );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'success': success,
+      'data': {
+        'upload': quote.toJson(),
+        'message': message,
+        'url': url,
+        'size': size,
+      },
+    };
   }
 }
