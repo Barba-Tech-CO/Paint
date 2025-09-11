27048e8b3a95acc913b653e542416256f0e8791b
Barba2k2 <lorenzo.dz@hotmail.com>
2025-09-11 18:41:21 -0300

    refactor: update quote service

--- lib/service/quote_service.dart
+++ lib/service/quote_service.dart
@@ -4,7 +4,9 @@ import 'dart:io';
 import 'package:dio/dio.dart';
 
 import '../config/app_urls.dart';
-import '../model/models.dart';
+import '../model/quotes_data/quote_list_response.dart';
+import '../model/quotes_data/quote_model.dart';
+import '../model/quotes_data/quote_response.dart';
 import '../model/quotes_data/extracted_material_model.dart';
 import '../utils/result/result.dart';
 import 'http_service.dart';
