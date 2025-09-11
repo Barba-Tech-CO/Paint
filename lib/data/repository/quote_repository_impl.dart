19f4fea92dcc10d4abc45bb2be1041100fb2d6a8
Barba2k2 <lorenzo.dz@hotmail.com>
2025-09-11 19:55:32 -0300

    refactor: update quote repository implementation

--- lib/data/repository/quote_repository_impl.dart
+++ lib/data/repository/quote_repository_impl.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
 import '../../domain/repository/quote_repository.dart';
-import '../../model/models.dart';
 import '../../model/quotes_data/extracted_material_model.dart';
+import '../../model/quotes_data/quote_list_response.dart';
+import '../../model/quotes_data/quote_model.dart';
+import '../../model/quotes_data/quote_response.dart';
 import '../../service/quote_service.dart';
 import '../../utils/result/result.dart';
 
