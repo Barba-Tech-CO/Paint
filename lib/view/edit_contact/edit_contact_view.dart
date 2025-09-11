77396f8c06706f634c0cc7594443785bff88015e
Barba2k2 <lorenzo.dz@hotmail.com>
2025-09-11 18:42:16 -0300

    refactor: update edit contact view

--- lib/view/edit_contact/edit_contact_view.dart
+++ lib/view/edit_contact/edit_contact_view.dart
@@ -8,10 +8,8 @@ import 'package:validatorless/validatorless.dart';
 
 import '../../config/app_colors.dart';
 import '../../helpers/snackbar_helper.dart';
-import '../../model/models.dart';
+import '../../model/contacts/contact_model.dart';
 import '../../viewmodel/contact/contact_detail_viewmodel.dart';
-import '../widgets/form_field/paint_pro_number_field.dart';
-import '../widgets/form_field/paint_pro_text_field.dart';
 import '../widgets/section_title_widget.dart';
 import '../widgets/widgets.dart';
 
