112ee236b01a5b25cc13981c9fe3baaac136d6f6
Barba2k2 <lorenzo.dz@hotmail.com>
2025-09-11 18:41:54 -0300

    refactor: update quote upload use case

--- lib/use_case/quotes/quote_upload_use_case.dart
+++ lib/use_case/quotes/quote_upload_use_case.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
 import '../../domain/repository/quote_repository.dart';
-import '../../model/models.dart';
+import '../../model/quotes_data/quote_list_response.dart';
+import '../../model/quotes_data/quote_model.dart';
+import '../../model/quotes_data/quote_response.dart';
 import '../../utils/logger/app_logger.dart';
 import '../../utils/result/result.dart';
 
