3ba761de729d3657be421868acefbcf157f2285c
Barba2k2 <lorenzo.dz@hotmail.com>
2025-09-11 18:42:01 -0300

    refactor: update auth content view

--- lib/view/auth/auth_content.dart
+++ lib/view/auth/auth_content.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../../model/models.dart';
+import '../../model/auth_model/auth_model.dart';
 import '../../utils/command/command_builder.dart';
 import '../../viewmodel/auth/auth_viewmodel.dart';
 import '../widgets/overlays/error_overlay.dart';
