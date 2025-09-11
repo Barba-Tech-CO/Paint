3de32e6c300571879caad263d00b1f8a55cbd8e0
Barba2k2 <lorenzo.dz@hotmail.com>
2025-09-11 18:45:54 -0300

    feat: add zone data response model for estimates

---
+++ lib/model/estimates/zone_data_response.dart
@@ -0,0 +1,27 @@
+import 'floor_dimensions.dart';
+import 'surface_areas.dart';
+
+/// Zone data response from backend
+class ZoneDataResponse {
+  final FloorDimensions floorDimensions;
+  final SurfaceAreas surfaceAreas;
+  final List<String> photos;
+
+  ZoneDataResponse({
+    required this.floorDimensions,
+    required this.surfaceAreas,
+    required this.photos,
+  });
+
+  factory ZoneDataResponse.fromJson(Map<String, dynamic> json) {
+    return ZoneDataResponse(
+      floorDimensions: FloorDimensions.fromJson(json['floor_dimensions'] ?? {}),
+      surfaceAreas: SurfaceAreas.fromJson(json['surface_areas'] ?? {}),
+      photos:
+          (json['photos'] as List<dynamic>?)
+              ?.map((p) => p.toString())
+              .toList() ??
+          [],
+    );
+  }
+}
