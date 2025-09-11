1467f05783ce9ca697e82268f31c38ba40276e04
Barba2k2 <lorenzo.dz@hotmail.com>
2025-09-11 18:41:07 -0300

    refactor: update estimate service

--- lib/service/estimate_service.dart
+++ lib/service/estimate_service.dart
@@ -1,12 +1,14 @@
 import 'package:dio/dio.dart';
 
-import '../model/models.dart';
+import '../model/estimates/estimate_create_request.dart';
+import '../model/estimates/estimate_model.dart';
+import '../model/estimates/estimate_response.dart';
 import '../utils/result/result.dart';
 import 'http_service.dart';
 
 class EstimateService {
   final HttpService _httpService;
-  static const String _baseUrl = '/paint-pro';
+  static const String _baseUrl = '/estimates';
 
   EstimateService(this._httpService);
 
@@ -208,6 +210,212 @@ class EstimateService {
     } catch (e) {
       return Result.error(
         Exception('Error sending estimate to GHL: $e'),
+      );
+    }
+  }
+
+  /// Cria um estimate completo com upload multipart
+  Future<Result<EstimateModel>> createEstimateMultipart(
+    EstimateCreateRequest request,
+  ) async {
+    try {
+      final form = FormData();
+
+      void addField(String key, String value) {
+        form.fields.add(MapEntry(key, value));
+      }
+
+      void addNumber(String key, num value) {
+        addField(key, value.toString());
+      }
+
+      Future<void> addFile(String key, String path, {String? filename}) async {
+        form.files.add(
+          MapEntry(
+            key,
+            await MultipartFile.fromFile(
+              path,
+              filename: filename ?? path.split('/').last,
+            ),
+          ),
+        );
+      }
+
+      // Campos principais
+      addField('contact_id', request.contactId);
+      addField('project_name', request.projectName);
+      addField('additional_notes', request.additionalNotes ?? '');
+
+      for (var zi = 0; zi < request.zones.length; zi++) {
+        final z = request.zones[zi];
+        addField('zones[$zi][id]', z.id);
+        addField('zones[$zi][name]', z.name);
+        if (zi == 0 && z.zoneType != null) {
+          addField(
+            'zones[$zi][zone_type]',
+            z.zoneType!.name,
+          ); // interior|exterior|both
+        }
+
+        // data[0]
+        const di = 0;
+        final fd = z.floorDimensions;
+        addNumber('zones[$zi][data][$di][floor_dimensions][length]', fd.length);
+        addNumber('zones[$zi][data][$di][floor_dimensions][width]', fd.width);
+        addNumber('zones[$zi][data][$di][floor_dimensions][height]', fd.height);
+        addField('zones[$zi][data][$di][floor_dimensions][unit]', 'ft');
+
+        // surface_areas (walls)
+        for (var wi = 0; wi < z.surfaceAreas.walls.length; wi++) {
+          final w = z.surfaceAreas.walls[wi];
+          addField(
+            'zones[$zi][data][$di][surface_areas][walls][$wi][id]',
+            w.id,
+          );
+          if (w.width != null) {
+            addNumber(
+              'zones[$zi][data][$di][surface_areas][walls][$wi][width]',
+              w.width!,
+            );
+          }
+          if (w.height != null) {
+            addNumber(
+              'zones[$zi][data][$di][surface_areas][walls][$wi][height]',
+              w.height!,
+            );
+          }
+          if (w.openingsArea != null) {
+            addNumber(
+              'zones[$zi][data][$di][surface_areas][walls][$wi][openings_area]',
+              w.openingsArea!,
+            );
+          }
+          if (w.netArea != null) {
+            addNumber(
+              'zones[$zi][data][$di][surface_areas][walls][$wi][net_area]',
+              w.netArea!,
+            );
+          }
+          addField(
+            'zones[$zi][data][$di][surface_areas][walls][$wi][unit]',
+            'sqft',
+          );
+        }
+
+        // surface_areas (ceiling)
+        for (var ci = 0; ci < z.surfaceAreas.ceiling.length; ci++) {
+          final c = z.surfaceAreas.ceiling[ci];
+          addField(
+            'zones[$zi][data][$di][surface_areas][ceiling][$ci][id]',
+            c.id,
+          );
+          if (c.width != null) {
+            addNumber(
+              'zones[$zi][data][$di][surface_areas][ceiling][$ci][width]',
+              c.width!,
+            );
+          }
+          if (c.height != null) {
+            addNumber(
+              'zones[$zi][data][$di][surface_areas][ceiling][$ci][height]',
+              c.height!,
+            );
+          }
+          if (c.openingsArea != null) {
+            addNumber(
+              'zones[$zi][data][$di][surface_areas][ceiling][$ci][openings_area]',
+              c.openingsArea!,
+            );
+          }
+          if (c.netArea != null) {
+            addNumber(
+              'zones[$zi][data][$di][surface_areas][ceiling][$ci][net_area]',
+              c.netArea!,
+            );
+          }
+          addField(
+            'zones[$zi][data][$di][surface_areas][ceiling][$ci][unit]',
+            'sqft',
+          );
+        }
+
+        // surface_areas (trim)
+        for (var ti = 0; ti < z.surfaceAreas.trim.length; ti++) {
+          final t = z.surfaceAreas.trim[ti];
+          addField('zones[$zi][data][$di][surface_areas][trim][$ti][id]', t.id);
+          if (t.width != null) {
+            addNumber(
+              'zones[$zi][data][$di][surface_areas][trim][$ti][width]',
+              t.width!,
+            );
+          }
+          if (t.height != null) {
+            addNumber(
+              'zones[$zi][data][$di][surface_areas][trim][$ti][height]',
+              t.height!,
+            );
+          }
+          if (t.openingsArea != null) {
+            addNumber(
+              'zones[$zi][data][$di][surface_areas][trim][$ti][openings_area]',
+              t.openingsArea!,
+            );
+          }
+          if (t.netArea != null) {
+            addNumber(
+              'zones[$zi][data][$di][surface_areas][trim][$ti][net_area]',
+              t.netArea!,
+            );
+          }
+          addField(
+            'zones[$zi][data][$di][surface_areas][trim][$ti][unit]',
+            'sqft',
+          );
+        }
+
+        // photos
+        for (final file in z.photos) {
+          await addFile('zones[$zi][data][$di][photos][]', file.path);
+        }
+      }
+
+      // materials
+      for (var mi = 0; mi < request.materials.length; mi++) {
+        final m = request.materials[mi];
+        addField('materials[$mi][id]', m.id);
+        addField('materials[$mi][unit]', m.unit);
+        addNumber('materials[$mi][quantity]', m.quantity);
+        addNumber('materials[$mi][unit_price]', m.unitPrice);
+        if (m.name != null) {
+          addField('materials[$mi][name]', m.name!);
+        }
+        if (m.description != null) {
+          addField('materials[$mi][description]', m.description!);
+        }
+      }
+
+      // totals
+      addNumber('totals[materials_cost]', request.totals.materialsCost);
+      addNumber('totals[grand_total]', request.totals.grandTotal);
+      if (request.totals.laborCost != null) {
+        addNumber('totals[labor_cost]', request.totals.laborCost!);
+      }
+      if (request.totals.additionalCosts != null) {
+        addNumber('totals[additional_costs]', request.totals.additionalCosts!);
+      }
+
+      final response = await _httpService.post(
+        _baseUrl, // apenas _baseUrl pois já aponta para /estimates
+        data: form,
+      );
+
+      final estimate = EstimateResponse.fromJson(
+        response.data['data'],
+      ).toEstimateModel();
+      return Result.ok(estimate);
+    } catch (e) {
+      return Result.error(
+        Exception('Error creating estimate with multipart: $e'),
       );
     }
   }
