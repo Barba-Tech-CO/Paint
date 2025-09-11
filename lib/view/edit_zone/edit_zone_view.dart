cdae94e5378ccf305199fe3aedc8b3779fa37aad
Barba2k2 <lorenzo.dz@hotmail.com>
2025-09-11 18:42:21 -0300

    refactor: update edit zone view

--- lib/view/edit_zone/edit_zone_view.dart
+++ lib/view/edit_zone/edit_zone_view.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../config/dependency_injection.dart';
-import '../../model/models.dart';
+import '../../model/projects/project_card_model.dart';
 import '../../utils/logger/app_logger.dart';
-import '../../viewmodel/zones/zones_viewmodels.dart';
+import '../../viewmodel/zones/zone_detail_viewmodel.dart';
+import '../../viewmodel/zones/zones_list_viewmodel.dart';
 import '../widgets/buttons/paint_pro_delete_button.dart';
 import '../widgets/widgets.dart';
 import 'widgets/floor_dimension_widget.dart';
