326e8df497f048384ee2de18b3fcfacd812d8f64
Barba2k2 <lorenzo.dz@hotmail.com>
2025-09-11 22:07:49 -0300

    feat(model): add surface areas model

---
+++ lib/model/estimates/surface_areas_model.dart
@@ -0,0 +1,7 @@
+class SurfaceAreasModel {
+  /// Áreas por superfície (chaves livres: walls, ceiling, trim, etc.)
+  final Map<String, num> values;
+
+  const SurfaceAreasModel({required this.values});
+}
+
