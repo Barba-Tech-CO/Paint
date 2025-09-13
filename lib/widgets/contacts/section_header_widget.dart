583555f25fef3d619edb4a3d0e81efad168e6328
Barba2k2 <lorenzo.dz@hotmail.com>
2025-09-13 15:27:23 -0300

    feat: update section header widget

--- lib/widgets/contacts/section_header_widget.dart
+++ lib/widgets/contacts/section_header_widget.dart
@@ -1,36 +1,30 @@
 import 'package:flutter/material.dart';
 
+import '../../config/app_colors.dart';
+
 class SectionHeaderWidget extends StatelessWidget {
   final IconData icon;
   final String title;
-  const SectionHeaderWidget({super.key, required this.icon, required this.title});
+  const SectionHeaderWidget({
+    super.key,
+    required this.icon,
+    required this.title,
+  });
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Padding(
       padding: const EdgeInsets.only(bottom: 8.0),
-      child: Row(
-        children: [
-          Icon(
-            icon,
-            color: theme.primaryColor,
-            size: 20,
-          ),
-          const SizedBox(width: 8),
-          Flexible(
-            child: Text(
-              title,
-              overflow: TextOverflow.ellipsis,
-              style: theme.textTheme.titleMedium?.copyWith(
-                fontWeight: FontWeight.bold,
-                color: theme.primaryColor,
-              ),
-            ),
-          ),
-        ],
+      child: Text(
+        title,
+        overflow: TextOverflow.ellipsis,
+        style: theme.textTheme.titleMedium?.copyWith(
+          fontWeight: FontWeight.w800,
+          fontSize: 18,
+          color: AppColors.primaryDark,
+        ),
       ),
     );
   }
 }
-
