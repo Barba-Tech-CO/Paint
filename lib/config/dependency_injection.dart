7c21d55a76a02e95ce2fe129f1a15f9823bfb0a4
Barba2k2 <lorenzo.dz@hotmail.com>
2025-09-14 10:53:59 -0300

    chore(config): Fix import order to alphabetical

--- lib/config/dependency_injection.dart
+++ lib/config/dependency_injection.dart
@@ -20,17 +20,17 @@ import '../domain/repository/quote_repository.dart';
 import '../service/app_initialization_service.dart';
 import '../service/auth_service.dart';
 import '../service/auth_persistence_service.dart';
+import '../service/camera_service.dart';
 import '../service/contact_service.dart';
 import '../service/contact_database_service.dart';
 import '../service/deep_link_service.dart';
 import '../service/estimate_service.dart';
 import '../service/http_service.dart';
 import '../service/location_service.dart';
-import '../service/navigation_service.dart';
 import '../service/material_service.dart';
+import '../service/navigation_service.dart';
 import '../service/paint_catalog_service.dart';
 import '../service/photo_service.dart';
-import '../service/camera_service.dart';
 import '../service/quote_service.dart';
 import '../service/user_service.dart';
 import '../helpers/estimate_builder.dart';
@@ -46,31 +46,31 @@ import '../use_case/auth/handle_deep_link_use_case.dart';
 import '../use_case/auth/handle_webview_navigation_use_case.dart';
 import '../use_case/contacts/contact_operations_use_case.dart';
 import '../use_case/contacts/contact_sync_use_case.dart';
-import '../use_case/quotes/quote_upload_use_case.dart';
 import '../use_case/estimates/estimate_upload_use_case.dart';
+import '../use_case/quotes/quote_upload_use_case.dart';
 
 // ViewModel Layer
 import '../viewmodel/select_colors_viewmodel.dart';
 import '../viewmodel/auth/auth_viewmodel.dart';
 import '../viewmodel/contact/contact_list_viewmodel.dart';
 import '../viewmodel/contact/contact_detail_viewmodel.dart';
+import '../viewmodel/contacts/contacts_viewmodel.dart';
 import '../viewmodel/estimate/estimate_list_viewmodel.dart';
 import '../viewmodel/estimate/estimate_detail_viewmodel.dart';
 import '../viewmodel/estimate/estimate_upload_viewmodel.dart';
 import '../viewmodel/estimate/estimate_calculation_viewmodel.dart';
+import '../viewmodel/material/material_list_viewmodel.dart';
+import '../viewmodel/measurements/measurements_viewmodel.dart';
+import '../viewmodel/navigation_viewmodel.dart';
 import '../viewmodel/paint_catalog/paint_catalog_list_viewmodel.dart';
 import '../viewmodel/paint_catalog/paint_catalog_detail_viewmodel.dart';
-import '../viewmodel/navigation_viewmodel.dart';
-import '../viewmodel/measurements/measurements_viewmodel.dart';
+import '../viewmodel/projects/projects_viewmodel.dart';
+import '../viewmodel/quotes/quotes_viewmodel.dart';
+import '../viewmodel/user/user_viewmodel.dart';
 import '../viewmodel/zones/zones_list_viewmodel.dart';
 import '../viewmodel/zones/zone_detail_viewmodel.dart';
 import '../viewmodel/zones/zones_summary_viewmodel.dart';
 import '../viewmodel/zones/zones_card_viewmodel.dart';
-import '../viewmodel/material/material_list_viewmodel.dart';
-import '../viewmodel/user/user_viewmodel.dart';
-import '../viewmodel/quotes/quotes_viewmodel.dart';
-import '../viewmodel/contacts/contacts_viewmodel.dart';
-import '../viewmodel/projects/projects_viewmodel.dart';
 
 final GetIt getIt = GetIt.instance;
 
@@ -167,7 +167,6 @@ void setupDependencyInjection() {
     () => ContactRepository(
       contactService: getIt<ContactService>(),
       databaseService: getIt<ContactDatabaseService>(),
-      authService: getIt<AuthService>(),
       logger: getIt<AppLogger>(),
       locationService: getIt<LocationService>(),
     ),
