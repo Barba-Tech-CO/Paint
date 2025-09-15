92c3e052f5fbfaecd28616986ade80c89ed58a44
Barba2k2 <lorenzo.dz@hotmail.com>
2025-09-15 13:13:23 -0300

    feat(zones): update zone data classes

--- lib/helpers/zones/zone_data_classes.dart
+++ lib/helpers/zones/zone_data_classes.dart
@@ -1,5 +1,3 @@
-// Helper classes for zone operations
-
 class ZoneRenameData {
   final int zoneId;
   final String newName;
@@ -7,21 +5,5 @@ class ZoneRenameData {
   ZoneRenameData({
     required this.zoneId,
     required this.newName,
-  });
-}
-
-class ZoneAddData {
-  final String title;
-  final String image;
-  final String floorDimensionValue;
-  final String floorAreaValue;
-  final String areaPaintable;
-
-  ZoneAddData({
-    required this.title,
-    required this.image,
-    required this.floorDimensionValue,
-    required this.floorAreaValue,
-    required this.areaPaintable,
   });
 }
