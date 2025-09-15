adb5a8e79c36e39d7165d2976a2ac736c7f532c4
Barba2k2 <lorenzo.dz@hotmail.com>
2025-09-15 20:26:35 -0300

    feat: update zones list widget

--- lib/widgets/zones/zones_list_widget.dart
+++ lib/widgets/zones/zones_list_widget.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
-import '../../config/dependency_injection.dart';
-import '../../viewmodel/zones/zone_detail_viewmodel.dart';
+import '../../helpers/zones/zones_list_helper.dart';
 import '../../viewmodel/zones/zones_list_viewmodel.dart';
 import '../cards/zones_card.dart';
 
@@ -28,15 +26,27 @@ class ZonesListWidget extends StatelessWidget {
                   children: [
                     ZonesCard(
                       title: zone.title,
-                      image: zone.image,
+                      photoPaths: ZonesListHelper.extractPhotoPaths(zone),
                       valueDimension: zone.floorDimensionValue,
                       valueArea: zone.floorAreaValue,
                       valuePaintable: zone.areaPaintable,
-                      onTap: () => _onZoneTap(context, zone),
-                      onEdit: () => _onZoneEdit(context, zone),
-                      onRename: (newName) =>
-                          _onZoneRename(context, zone, newName),
-                      onDelete: () => _onZoneDelete(context, zone),
+                      onTap: () => ZonesListHelper.onZoneTap(
+                        context,
+                        zone,
+                        listViewModel,
+                      ),
+                      onEdit: () => ZonesListHelper.onZoneEdit(context, zone),
+                      onRename: (newName) => ZonesListHelper.onZoneRename(
+                        context,
+                        zone,
+                        newName,
+                        listViewModel,
+                      ),
+                      onDelete: () => ZonesListHelper.onZoneDelete(
+                        context,
+                        zone,
+                        listViewModel,
+                      ),
                     ),
                     const SizedBox(height: 16),
                   ],
@@ -48,36 +58,5 @@ class ZonesListWidget extends StatelessWidget {
         ),
       ),
     );
-  }
-
-  void _onZoneTap(BuildContext context, dynamic zone) {
-    listViewModel.selectZone(zone);
-    context.push('/zones-details', extra: zone);
-  }
-
-  void _onZoneEdit(BuildContext context, dynamic zone) {
-    context.push('/edit-zone', extra: zone);
-  }
-
-  void _onZoneRename(BuildContext context, dynamic zone, String newName) {
-    final detailViewModel = getIt<ZoneDetailViewModel>();
-    detailViewModel.setCurrentZone(zone);
-
-    detailViewModel.onZoneUpdated = (updatedZone) {
-      listViewModel.updateZone(updatedZone);
-    };
-
-    detailViewModel.renameZone(zone.id, newName);
-  }
-
-  void _onZoneDelete(BuildContext context, dynamic zone) {
-    final detailViewModel = getIt<ZoneDetailViewModel>();
-    detailViewModel.setCurrentZone(zone);
-
-    detailViewModel.onZoneDeleted = (deletedZoneId) {
-      listViewModel.removeZone(deletedZoneId);
-    };
-
-    detailViewModel.deleteZone(zone.id);
   }
 }
