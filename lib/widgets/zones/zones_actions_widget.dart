e8e952864c25acc38eeb238f8bd942de00c3cf12
Barba2k2 <lorenzo.dz@hotmail.com>
2025-09-15 13:14:45 -0300

    feat(zones): add zones actions widget

---
+++ lib/widgets/zones/zones_actions_widget.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../viewmodel/zones/zones_list_viewmodel.dart';
+import '../../viewmodel/zones/zones_summary_viewmodel.dart';
+import '../buttons/paint_pro_button.dart';
+import 'add_zone_dialog.dart';
+import 'zones_summary_card.dart';
+
+class ZonesActionsWidget extends StatelessWidget {
+  final ZonesListViewModel listViewModel;
+  final ZonesSummaryViewModel summaryViewModel;
+
+  const ZonesActionsWidget({
+    super.key,
+    required this.listViewModel,
+    required this.summaryViewModel,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        if (summaryViewModel.summary != null)
+          ZonesSummaryCard(
+            avgDimensions: summaryViewModel.summary!.avgDimensions,
+            totalArea: summaryViewModel.summary!.totalArea,
+            totalPaintable: summaryViewModel.summary!.totalPaintable,
+            onAdd: () => _showAddZoneDialog(context),
+          ),
+        const SizedBox(height: 32),
+        PaintProButton(
+          text: "Next",
+          onPressed: () => context.push('/select-material'),
+        ),
+        const SizedBox(height: 16),
+      ],
+    );
+  }
+
+  void _showAddZoneDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => AddZoneDialog(
+        onAdd:
+            ({
+              required String title,
+              required String zoneType,
+            }) {
+              context.pop();
+
+              WidgetsBinding.instance.addPostFrameCallback((_) {
+                if (context.mounted) {
+                  // Navegar para a câmera/RoomPlan para capturar os dados
+                  context.go('/camera');
+                }
+              });
+            },
+      ),
+    );
+  }
+}
