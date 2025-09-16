03e9c24aad87bb56e20fbb8b7adb5f79e57decee
Barba2k2 <lorenzo.dz@hotmail.com>
2025-09-16 18:40:53 -0300

    refactor(model): update floor dimensions model

--- lib/model/estimates/floor_dimensions_model.dart
+++ lib/model/estimates/floor_dimensions_model.dart
@@ -1,13 +1,11 @@
 class FloorDimensionsModel {
   final num length;
-  final num width;
-  final num height;
+  final num width; 
   final String unit; // ex: ft, m
 
   const FloorDimensionsModel({
     required this.length,
     required this.width,
-    required this.height,
     this.unit = 'ft',
   });
 }
