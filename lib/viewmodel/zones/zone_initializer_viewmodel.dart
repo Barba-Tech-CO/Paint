8bf18758c63f9ae22947c9e334ebd40e718e8e4f
Barba2k2 <lorenzo.dz@hotmail.com>
2025-09-17 11:16:57 -0300

    feat(zones): update zone initializer viewmodel

--- lib/viewmodel/zones/zone_initializer_viewmodel.dart
+++ lib/viewmodel/zones/zone_initializer_viewmodel.dart
@@ -8,17 +8,28 @@ class ZoneInitializerViewModel {
   ZoneInitializerViewModel({required this.listViewModel});
 
   void initializeZone(Map<String, dynamic> zoneData) {
-    listViewModel.addZone(
-      title: zoneData['title'],
-      floorDimensionValue: zoneData['floorDimensionValue'],
-      floorAreaValue: zoneData['floorAreaValue'],
-      areaPaintable: zoneData['areaPaintable'],
-      image: zoneData['image'],
-      ceilingArea: zoneData['ceilingArea'],
-      trimLength: zoneData['trimLength'],
-      // Store additional RoomPlan data for zone editing
-      roomPlanData: zoneData['roomPlanData'],
-    );
+    // Check if zone already exists to avoid duplicates
+    try {
+      listViewModel.zones.firstWhere(
+        (zone) =>
+            zone.title == zoneData['title'] && zone.image == zoneData['image'],
+      );
+      // Zone already exists, don't add it again
+      return;
+    } catch (e) {
+      // Zone doesn't exist, add it
+      listViewModel.addZone(
+        title: zoneData['title'],
+        floorDimensionValue: zoneData['floorDimensionValue'],
+        floorAreaValue: zoneData['floorAreaValue'],
+        areaPaintable: zoneData['areaPaintable'],
+        image: zoneData['image'],
+        ceilingArea: zoneData['ceilingArea'],
+        trimLength: zoneData['trimLength'],
+        // Store additional RoomPlan data for zone editing
+        roomPlanData: zoneData['roomPlanData'],
+      );
+    }
   }
 
   void initializeZoneAfterBuild(
