eef7e302190c1e9d878207779a7a3d013d891fa8
Barba2k2 <lorenzo.dz@hotmail.com>
2025-09-18 16:44:20 -0300

    feat: update material repository interface

--- lib/domain/repository/material_repository.dart
+++ lib/domain/repository/material_repository.dart
@@ -1,14 +1,20 @@
 import '../../model/material_models/material_model.dart';
 import '../../model/material_models/material_stats_model.dart';
 import '../../utils/result/result.dart';
+
 abstract class IMaterialRepository {
   /// Busca todos os materiais disponíveis
-  Future<Result<List<MaterialModel>>> getAllMaterials();
+  Future<Result<List<MaterialModel>>> getAllMaterials({
+    int? limit,
+    int? offset,
+  });
 
   /// Busca materiais com filtros aplicados
   Future<Result<List<MaterialModel>>> getMaterialsWithFilter(
-    MaterialFilter filter,
-  );
+    MaterialFilter filter, {
+    int? limit,
+    int? offset,
+  });
 
   /// Busca material por ID
   Future<Result<MaterialModel?>> getMaterialById(String id);
