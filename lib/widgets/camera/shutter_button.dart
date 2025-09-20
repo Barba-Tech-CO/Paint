1641ee49eb7d06a5e1622b315c5dd3b9e89ec3c4
Barba2k2 <lorenzo.dz@hotmail.com>
2025-09-20 11:17:02 -0300

    feat(widget): update shutter button implementation

--- lib/widgets/camera/shutter_button.dart
+++ lib/widgets/camera/shutter_button.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 import '../../config/app_colors.dart';
@@ -34,7 +36,12 @@ class ShutterButton extends StatelessWidget {
         : innerColor;
 
     return GestureDetector(
-      onTap: isDisabled ? null : onTap,
+      onTap: isDisabled
+          ? null
+          : () {
+              log('ShutterButton: onTap called');
+              onTap?.call();
+            },
       child: Container(
         width: outerSize,
         height: outerSize,
