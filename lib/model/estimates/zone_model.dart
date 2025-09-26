d25876f2c5bce80b8dce6363f6350f4ffa037046
Barba2k2 <lorenzo.dz@hotmail.com>
2025-09-26 00:14:45 -0300

    refactor: update zone model

--- lib/model/estimates/zone_model.dart
+++ lib/model/estimates/zone_model.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'zone_data_model.dart';
 
 class ZoneModel {
@@ -12,4 +13,38 @@ class ZoneModel {
     required this.zoneType,
     required this.data,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'zone_type': zoneType,
+      'data': data.map((d) => d.toMap()).toList(),
+    };
+  }
+
+  factory ZoneModel.fromMap(Map<String, dynamic> map) {
+    final rawData = map['data'];
+    List<dynamic> dataList;
+    if (rawData is List) {
+      dataList = rawData;
+    } else if (rawData is String) {
+      try {
+        dataList = (jsonDecode(rawData) as List);
+      } catch (_) {
+        dataList = const [];
+      }
+    } else {
+      dataList = const [];
+    }
+
+    return ZoneModel(
+      id: map['id']?.toString(),
+      name: (map['name'] ?? '').toString(),
+      zoneType: (map['zone_type'] ?? '').toString(),
+      data: dataList
+          .map((d) => ZoneDataModel.fromMap(d as Map<String, dynamic>))
+          .toList(),
+    );
+  }
 }
