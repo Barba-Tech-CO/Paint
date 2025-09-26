c21d3749f2a82449f15e4ae81d78ce4090403690
Barba2k2 <lorenzo.dz@hotmail.com>
2025-09-26 00:14:38 -0300

    refactor: update surface areas model

--- lib/model/estimates/surface_areas_model.dart
+++ lib/model/estimates/surface_areas_model.dart
@@ -3,4 +3,23 @@ class SurfaceAreasModel {
   final Map<String, num> values;
 
   const SurfaceAreasModel({required this.values});
+
+  Map<String, dynamic> toMap() {
+    return Map<String, dynamic>.from(values);
+  }
+
+  factory SurfaceAreasModel.fromMap(Map<String, dynamic> map) {
+    final out = <String, num>{};
+    map.forEach((key, value) {
+      if (value is num) {
+        out[key] = value;
+      } else if (value is String) {
+        final parsed = double.tryParse(value);
+        if (parsed != null) {
+          out[key] = parsed;
+        }
+      }
+    });
+    return SurfaceAreasModel(values: out);
+  }
 }
