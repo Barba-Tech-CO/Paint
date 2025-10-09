69af8404eb7627e6681cab6f05a7a05df4f34504
Barba2k2 <lorenzo.dz@hotmail.com>
2025-10-09 08:46:45 -0300

    feat(widgets): add paint pro drawer

---
+++ lib/widgets/drawer/paint_pro_drawer.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import '../../config/app_colors.dart';
+import '../../viewmodel/user/user_viewmodel.dart';
+import '../../viewmodel/home/home_viewmodel.dart';
+import 'connect_ghl_menu_item_widget.dart';
+import 'drawer_header_content_widget.dart';
+import 'logout_menu_item_widget.dart';
+
+class PaintProDrawer extends StatelessWidget {
+  final UserViewModel? userViewModel;
+  final HomeViewModel? homeViewModel;
+
+  const PaintProDrawer({
+    super.key,
+    this.userViewModel,
+    this.homeViewModel,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: AppColors.background,
+      elevation: 0,
+      child: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(height: 24),
+            DrawerHeaderContentWidget(
+              userViewModel: userViewModel,
+              homeViewModel: homeViewModel,
+            ),
+            const SizedBox(height: 32),
+            Expanded(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: const [
+                  ConnectGhlMenuItemWidget(),
+                  SizedBox(height: 8),
+                  LogoutMenuItemWidget(),
+                ],
+              ),
+            ),
+            const SizedBox(height: 24),
+          ],
+        ),
+      ),
+    );
+  }
+}
